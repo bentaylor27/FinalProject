1ab0ec3c29bff1ef737c17fa9b0ffd2fd131214a
user <user@users-MacBook-Air.local>
2020-09-23 20:51:34 -0500

    Fizz Buzz answered

--- student_examples/answers.js
+++ student_examples/answers.js
@@ -1,10 +1,20 @@
+//EASY GOING
 // for (i = 1; i <= 20; i++)
 //     console.log(i)
 
-for (let i = 0; i <= 200; i+=2) {
-    console.log(i)
-}
-
-// for (let i = 20; i >=0; i-=2) {
+//GET EVEN
+// for (let i = 0; i <= 200; i+=2) {
 //     console.log(i)
 // }
+
+//FIZZ BUZZ
+for (i = 1; i <=100; i++) {
+    if ( i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else if (i % 3 && 5 == 0)
+        console.log("FizzBuzz");
+    else
+        console.log(i);
+}
