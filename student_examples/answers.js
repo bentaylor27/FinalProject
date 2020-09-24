9552d4b23a242c28454e853566e8596222d36f72
user <user@users-MacBook-Air.local>
2020-09-23 20:41:53 -0500

    Get Even answered

--- student_examples/answers.js
+++ student_examples/answers.js
@@ -1,2 +1,10 @@
-for (i = 1; i <= 20; i++)
+// for (i = 1; i <= 20; i++)
+//     console.log(i)
+
+for (let i = 0; i <= 200; i+=2) {
     console.log(i)
+}
+
+// for (let i = 20; i >=0; i-=2) {
+//     console.log(i)
+// }
