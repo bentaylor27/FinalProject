073a11365a8f365951612105b8656da1a5f128f5
user <user@users-Air.COMFAST>
2020-09-24 20:32:56 -0500

    sum array completed

--- student_examples/solutions.js
+++ student_examples/solutions.js
@@ -17,4 +17,21 @@ function checkPalindrome(string){
     return true
 }
 
+console.log(checkPalindrome(string));
 
+
+//SUM ARRAY
+var array=[1,2,3,4,5,6]
+
+function sumArray(array){
+    let sum=0;
+    for (let i=0; i<array.length; i++){
+        sum += array[i]
+    }
+    return sum
+}
+
+console.log(sumArray(array));
+
+
+//PRIME NUMBERS
