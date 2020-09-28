57aad4034ccc5802e1dba7a1835914355b9f2b2c
user <user@users-MacBook-Air.local>
2020-09-28 17:16:22 -0500

    crazy object complete

--- student_examples/homework/answers.js
+++ student_examples/homework/answers.js
@@ -25,11 +25,58 @@
 
 // console.log(rainbow[4])
 
-const ben = {
-    food: 'cheeseburgers',
-    hobby: 'football',
-    town: 'Chicago',
-    datatype: 'Boolean'
-}
+// const ben = {
+//     food: 'cheeseburgers',
+//     hobby: 'football',
+//     town: 'Chicago',
+//     datatype: 'Boolean'
+// }
 
-console.log(ben.hobby);
+// console.log(ben.hobby);
+
+
+//CRAZY OBJECT!
+
+// const crazyObject = {
+//     taco: [{meat: 'steak',
+//            cheese: ['panela', 'queso', 'chihuahua']},
+//            {meat: 'chicken',
+//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//           ],
+//     larry: {
+//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//       characters: [{
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//       ]
+//     }
+//   }
+
+
+//console.log(crazyObject.taco[1].salsa[5])
+
+//console.log(crazyObject.larry.quotes[0])
+
+//console.log(crazyObject.larry.characters[2].favourtieHobby)
+
+//console.log(crazyObject.larry.nicknames[1])
+
+//console.log(crazyObject.larry.characters[1])
+
+// crazyObject.larry.quotes = "I'm trying to elevate small talk to medium talk"
+//   console.log(crazyObject.larry.quotes)
+
+
+
+//OBJECT-CEPTION
