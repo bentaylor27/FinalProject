30c68ca59dbb0ec4cc932c979df2bddbae1ca7dc
user <user@users-MacBook-Air.local>
2020-09-28 15:52:28 -0500

    dataype refresher complete

---
+++ student_examples/homework/answers.js
@@ -0,0 +1,23 @@
+//DATATYPE REFRESHER
+//1. A light switch that can be either on or off.
+    //datatype: boolean
+    //example: const switchOn: true
+//2. A user's email address.
+    //datatype: string
+    //example: let email = bentaylor@gmail.com
+//3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    //datatype: object
+    //example: let spaceship = ('hull', 'laser blasters', 'tractor beam', 'warp drive')
+//4. A list of student names from our class.
+    //datatype: array
+    //example: const studentNames = [jeff, bob, billy, tom, etc.]
+//5. A list of student names from our class, each with a location.
+    //datatype: array
+    //example: let studentNames = [{name: ben, location: chicago}, {name: carly, location: houston}]
+//6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    //datatype: array
+    //example: let studentNames = [{name: ben, location: chicago, favorite show: South Park}, {name: carly, location: houston, favorite show: Friends}]
+
+
+
+//TAKE IT EASY
