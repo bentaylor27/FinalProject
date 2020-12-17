c695cf2de976ac59c78df4a2919b308bd3d5456d
user <user@users-MacBook-Air.local>
2020-12-16 22:51:49 -0600

    css template successfully implemented

--- student_examples/GA_Work/portfolio/src/App.js
+++ student_examples/GA_Work/portfolio/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import '../src/App.css'
+import '../src/responsiveness.css'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div className='body'>
+        {/* <div className='body'>
           <header>
             <h1>Hi, I'm Ben Taylor</h1>
             <nav className='navbar'>
@@ -21,7 +22,7 @@ export default class App extends Component {
         <footer className='footer'>
             <a href='https://www.linkedin.com/in/ben-taylor-se/' target='_blank' rel='noreferrer'>LinkedIn</a>
             <a href='https://github.com/bentaylor27' target='_blank' rel='noreferrer'>Github</a>
-        </footer>
+        </footer> */}
       </div>
     )
   }
